6ed0ee2ee65ea8963661ca28d7e7dfa96d3ced1b
Carlos Tiago <carlostiago@me.com>
2016-01-20 17:53:33 +0000

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,14 @@ CourseStatus.delete_all
 CourseStatus.create! id: 1, name: "Ativo"
 CourseStatus.create! id: 2, name: "Suspenso"
 
+Student.delete_all
+Student.create! id: 1, name: "Carlos Tiago", register_number: "ABC1", status: 1
+Student.create! id: 2, name: "Carlos", register_number: "ABC2", status: 2
+
+Course.delete_all
+Course.create! id: 1, name: "Web Design", description: "ABC1", status: 1
+Course.create! id: 2, name: "Programação", description: "ABC2", status: 2
+
+Classroom.delete_all
+Classroom.create! id: 1, course_id: 1, student_id: 2, entry_at: Time.now 
+Classroom.create! id: 2, course_id: 2, student_id: 2, entry_at: Time.now 
